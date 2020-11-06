dd2b9eeb935c8106ce6bd4a54d53cd6acd0d1ba2
Denis Kuimov <deniskuimov@mail.ru>
2020-11-06 23:39:10 +0500

    Create repo

---
+++ vue-app/src/layouts/MainLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Navbar @click="isOpen = !isOpen" />
+    <Sidebar v-model="isOpen" />
+    <div class="app-main-layout">
+    <main class="app-content" :class="{full: !isOpen}">
+      <div class="app-page">
+        <router-view/>
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <a class="btn-floating btn-large blue" href="#">
+        <i class="large material-icons">add</i>
+      </a>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import Navbar from '@/components/app/Navbar'
+import Sidebar from '@/components/app/Sidebar'
+
+export default {
+  name: 'main-layout',
+  data: () => ({
+    isOpen: true
+  }),
+  components: {
+    Navbar, Sidebar
+  }
+}
+</script>
