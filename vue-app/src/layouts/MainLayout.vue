c7fab563b0f994a0c1f5cc2e46f4544bb14e2457
Denis Kuimov <deniskuimov@mail.ru>
2020-11-18 21:47:23 +0500

    Added componenet HomeBill and HomeCurreucy

--- vue-app/src/layouts/MainLayout.vue
+++ vue-app/src/layouts/MainLayout.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
-    <Navbar @click="isOpen = !isOpen" />
-    <Sidebar v-model="isOpen" />
-    <div class="app-main-layout">
-    <main class="app-content" :class="{full: !isOpen}">
-      <div class="app-page">
-        <router-view/>
-      </div>
-    </main>
+    <Loader v-if="loading" />
+    <div class="app-main-layout" v-else>
 
-    <div class="fixed-action-btn">
-      <router-link class="btn-floating btn-large blue" to="/record">
-        <i class="large material-icons">add</i>
-      </router-link>
+      <Navbar @click="isOpen = !isOpen" />
+
+      <Sidebar v-model="isOpen" />
+
+      <main class="app-content" :class="{full: !isOpen}">
+        <div class="app-page">
+          <router-view/>
+        </div>
+      </main>
+
+      <div class="fixed-action-btn">
+        <router-link class="btn-floating btn-large blue" to="/record">
+          <i class="large material-icons">add</i>
+        </router-link>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -25,12 +29,15 @@ import Sidebar from '@/components/app/Sidebar'
 export default {
   name: 'main-layout',
   data: () => ({
-    isOpen: true
+    isOpen: true,
+    loading: true
   }),
   async mounted () {
     if (!Object.keys(this.$store.getters.info).length) {
       await this.$store.dispatch('fetchInfo')
     }
+
+    this.loading = false
   },
   components: {
     Navbar, Sidebar
